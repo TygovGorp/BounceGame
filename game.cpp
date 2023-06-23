e45fd3fa47f9d00250ad3bb573eba87203f39640
gamer013NL <Tygo.vanGorp@outlook.com>
2023-06-23 22:24:21 +0200

    mouse tracking added and menu navigation inputs added

--- game.cpp
+++ game.cpp
@@ -4,12 +4,14 @@
 
 namespace Tmpl8
 {
+	
 	// -----------------------------------------------------------
 	// Initialize the application
 	// -----------------------------------------------------------
 	void Game::Init()
-	{
-		
+	{	
+		//Game::MouseX = 0;
+		//Game::MouseY = 0;
 		/*
 		LoadLevel();
 		PlayerInit();
@@ -27,12 +29,14 @@ namespace Tmpl8
 
 	void Game::Tick(float deltaTime)
 	{
+		screen->Clear(0);
 		Update();
 		Render();
 	}
 
 	void Game::Update() 
 	{
+		
 		/*
 		KeyboardUpd()
 		MouseLocUpd()
@@ -50,5 +54,10 @@ namespace Tmpl8
 		ProjectileRen()
 		ScoreRen()
 		*/
+	}
+
+	void Game::CheckScreenBounds()
+	{
+		// Add logic to check and handle boundaries
 	}
 };
--- game.h
+++ game.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SDL.h>
+#include <iostream>
+#include <string> 
 
 namespace Tmpl8 {
 
@@ -13,37 +15,48 @@ public:
 	void Tick(float deltaTime);
 	void Update();
 	void Render();
+	void CheckScreenBounds();
 	void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
 	void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
-	void MouseMove( int x, int y ) { /* implement if you want to detect mouse movement */ }
+	void MouseMove(int x, int y) 
+	{
+		MouseX = x;
+		MouseY = y - 512;
+		MouseY = abs(MouseY);
+		std::cout << MouseX << "," << MouseY << std::endl;
+	}
 	void KeyUp( int key ) { /* implement if you want to handle keys */ }
-	void KeyDown( int key ) { /* implement if you want to handle keys */ }
+	void KeyDown(int key) 
+	{
+		/*	debug code for figuring out what the number was for each key
+		std::string str = std::to_string(key);
+		const char* test = str.c_str();
+		printf(test);
+		*/
+		// (main)menu navigation inputs
+		switch (key)
+		{
+		case 43: //tab
+			break;
+		case 82: //arrow up
+			break;
+		case 80: //arrow left
+			break;
+		case 81: //arrow down
+			break;
+		case 79: //arrow right
+			break;
+		default:
+			break;
+		}
+	}
 
 
 	static SDL_Renderer* renderer;
-	static SDL_Event event;
 	static const int ScreenSize = 768;
+	int MouseX;
+	int MouseY;
 private:
 	Surface* screen;
-};
-class Entity
-{
-public:
-	Entity() {}
-	~Entity() {}
-	void Init(int height, int width, int scale, int x, int y, const char* path)
-	{
-
-	}
-	void Update()
-	{
-
-	}
-	void Render()
-	{
-
-	}
-private:
-
 };
 }; // namespace Tmpl8
